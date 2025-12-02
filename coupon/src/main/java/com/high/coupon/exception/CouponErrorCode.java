93a60e0780d5052126172263495073a6f11668dc
Sona <objet_an@naver.com>
2025-12-02 13:27:54 +0900

    feat: [coupon] errorCode 추가
    - 쿠폰 6000번대 에러

--- coupon/src/main/java/com/high/coupon/exception/CouponErrorCode.java
+++ coupon/src/main/java/com/high/coupon/exception/CouponErrorCode.java
@@ -1,10 +1,30 @@
 package com.high.coupon.exception;
 
-public enum CouponErrorCode {
+import com.library.module.exception.BaseErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
-    // todo 작성 필요
+@Getter
+@RequiredArgsConstructor
+public enum CouponErrorCode implements BaseErrorCode {
+
+    /**
+     * COUPON - error 6000번대
+     */
 
     // application
+    COUPON_NOT_FOUND(4000, HttpStatus.NOT_FOUND, "쿠폰을 찾을 수 없습니다."),
+
 
     // domain
+
+
+
+    ;
+
+    private final int code;
+    private final HttpStatus status;
+    private final String message;
+
 }
