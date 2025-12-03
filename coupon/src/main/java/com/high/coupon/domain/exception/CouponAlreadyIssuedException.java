95a79d20c36c3ce0f42bbab103971625dcc744ef
Sona <objet_an@naver.com>
2025-12-03 12:07:26 +0900

    feat: [coupon] 쿠폰 에러코드 추가

--- coupon/src/main/java/com/high/coupon/domain/exception/CouponAlreadyIssuedException.java
+++ coupon/src/main/java/com/high/coupon/domain/exception/CouponAlreadyIssuedException.java
@@ -4,6 +4,7 @@ import com.high.coupon.exception.CouponErrorCode;
 import com.library.module.exception.CustomException;
 
 public class CouponAlreadyIssuedException extends CustomException {
+
     public CouponAlreadyIssuedException() {
         super(CouponErrorCode.COUPON_ALREADY_ISSUED);
     }
