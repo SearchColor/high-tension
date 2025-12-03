4d59f919bad27550e1411cbaa107912813312433
Sona <objet_an@naver.com>
2025-12-03 12:33:23 +0900

    refactor: [coupon] 할인률 DB 저장 범위 수정
    - 사용자 10 입력 -> DB 0.1 저장으로 /100 계산 불필요하도록 제거

--- coupon/src/main/java/com/high/coupon/domain/entity/Coupon.java
+++ coupon/src/main/java/com/high/coupon/domain/entity/Coupon.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.AccessLevel;
@@ -35,7 +36,7 @@ public class Coupon extends BaseEntity {
     private String description;
 
     @Column(nullable = false, precision = 5, scale = 2)
-    private BigDecimal discountRate;
+    private BigDecimal discountRate; // 0~1 범위 저장
 
     @Column(nullable = false)
     private Integer totalQuantity;
@@ -56,11 +57,12 @@ public class Coupon extends BaseEntity {
             Integer totalQuantity, LocalDateTime issueStartAt, LocalDateTime issueEndAt, LocalDateTime validUntil){
 
         validateDates(issueStartAt, issueEndAt, validUntil);
+        BigDecimal convertDiscountRate = convertAndValidateDiscountRate(discountRate);
 
         return Coupon.builder()
                 .name(name)
                 .description(description)
-                .discountRate(discountRate)
+                .discountRate(convertDiscountRate) // 10(입력) -> 0.1 저장 (0~1) 할인률 범위
                 .totalQuantity(totalQuantity)
                 .issueStartAt(issueStartAt)
                 .issueEndAt(issueEndAt)
@@ -79,5 +81,16 @@ public class Coupon extends BaseEntity {
             throw new IllegalArgumentException("발행 종료일은 시작일 이후여야 합니다.");
         if (validUntil.isBefore(issueStartAt))
             throw new IllegalArgumentException("유효기간 종료일은 발행 시작일 이후여야 합니다.");
+    }
+
+    private static BigDecimal convertAndValidateDiscountRate(BigDecimal rate) {
+        if (rate == null || rate.compareTo(BigDecimal.ZERO) <= 0 || rate.compareTo(BigDecimal.valueOf(100)) > 0) {
+            throw new IllegalArgumentException("할인율은 0~100 사이여야 합니다.");
+        }
+        if (rate.scale() > 0){
+            throw new IllegalArgumentException("할인율은 소수점 없이 1% 단위만 가능합니다.");
+        }
+
+        return rate.divide(BigDecimal.valueOf(100));
     }
 }
