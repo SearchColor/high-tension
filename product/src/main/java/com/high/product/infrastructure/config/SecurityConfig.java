0a9e791249771949f488bb3263ef33636438b683
sp-PJS <redvoca100466@gmail.com>
2025-12-08 11:31:04 +0900

    feat:유저 인가 구현

--- product/src/main/java/com/high/product/infrastructure/config/SecurityConfig.java
+++ product/src/main/java/com/high/product/infrastructure/config/SecurityConfig.java
@@ -1,13 +1,15 @@
 package com.high.product.infrastructure.config;
 
+import com.high.product.infrastructure.security.HeaderAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
+
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,17 +19,24 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http,
-		DefaultAuthenticationEventPublisher authenticationEventPublisher) throws Exception {
-		http
-			.csrf(AbstractHttpConfigurer::disable)
-			.sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-			.authorizeHttpRequests(auth -> auth
-				.requestMatchers("/api/v1/**").permitAll()
-				.anyRequest().authenticated()
+		DefaultAuthenticationEventPublisher publisher)
+		throws Exception {
 
+		http
+			.csrf(csrf -> csrf.disable())
+			.sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+			.addFilterBefore(new HeaderAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+
+			.authorizeHttpRequests(auth -> auth
+				.requestMatchers(
+					"/docs/**",
+					"/actuator/**"
+				).permitAll()
+
+				.anyRequest().authenticated()
 			);
 
 		return http.build();
 	}
-
 }
