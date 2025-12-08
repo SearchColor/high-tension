9efe3dab1e4cd9cad550457bfff923fa3dd12515
Sona <objet_an@naver.com>
2025-12-08 15:02:48 +0900

    feat: [coupon] kafka 컨슈머 구현

---
+++ coupon/src/main/java/com/high/coupon/infrastructure/kafka/consumer/KafkaConsumer.java
@@ -0,0 +1,59 @@
+package com.high.coupon.infrastructure.kafka.consumer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.high.coupon.application.CouponIssueService;
+import com.high.coupon.infrastructure.kafka.dto.CouponUseRequestMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Service;
+
+/**
+ * Kafka 토픽 구독 Consumer - coupon-use-request 토픽 메세지를 받음
+ * todo 연동 테스트를 위해 컨슈머 기본 로직만 작성 (성공/실패 응답 로직 필요 여부 확인)
+ * 실패 시 로그만 남기고 ack 처리하도록 구현
+ */
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class KafkaConsumer {
+
+    private final CouponIssueService couponIssueService;
+    private final ObjectMapper objectMapper;
+
+    /**
+     * 쿠폰 사용 전환 요청 메시지 구독
+     * - Infrastructure Layer
+     * - Kafka 토픽: coupon-use-request
+     * - Consumer Group: coupon-service-group
+     */
+    @KafkaListener(
+            topics = "coupon-use-request",
+            groupId = "coupon-service-group",
+            containerFactory = "kafkaListenerContainerFactory"
+    )
+    public void consumeCouponRequest(String message, Acknowledgment ack) {
+        log.info("[쿠폰 소비자] 수신 메시지: {}", message);
+        CouponUseRequestMessage dto = null;
+
+        try {
+            // JSON → DTO 역직렬화 (UUID 타입)
+            dto = objectMapper.readValue(message, CouponUseRequestMessage.class);
+            log.info("파싱 완료 sagaId: {}", dto.sagaId());
+
+            // Application Service 호출 (필요한 필드만 전달)
+            couponIssueService.useCoupon(dto.couponIssueId(), dto.userId());
+
+            // 처리 성공 시 알림
+            log.info("[쿠폰 소비자] 처리 성공 (sagaID: {}, couponIssueID: {})", dto.sagaId(), dto.couponIssueId());
+
+        } catch (Exception e) {
+            log.error("[쿠폰 소비자] 처리 실패 - 메시지 건너뜀. 내용: {}, 에러: {}", message, e.getMessage()); // todo: KafkaConfig DLQ 도입 필요
+
+        } finally {
+            // 성공하든 실패하든 무조건 커밋(Ack) 후 다음 메시지를 받을 준비
+            ack.acknowledge();
+        }
+    }
+}
--- coupon/src/main/java/com/high/coupon/infrastructure/kafka/dto/CouponUseRequestMessage.java
+++ coupon/src/main/java/com/high/coupon/infrastructure/kafka/dto/CouponUseRequestMessage.java
@@ -3,7 +3,6 @@ package com.high.coupon.infrastructure.kafka.dto;
 import java.util.UUID;
 
 /**
- * todo 필드 맞춰야함 (필요 필드 확인 필요)
  * Kafka 메세지 역직렬화를 위한 DTO
  * - Kafka에서 오는 json 메세지 구조와 일치 시켜야함
  * ObjectMapper (String -> UUID 변환)
