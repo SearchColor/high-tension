50f75183101aae3369aac701596b8b2e0e53bc87
GoodNyong <gy990213@gmail.com>
2025-12-10 06:04:01 +0900

    feat: Token 재발급 DTO 및 예외 추가
    - InvalidRefreshTokenException: INVALID_REFRESH_TOKEN(2202) 래핑
    - TokenReissueRequest: refreshToken 검증 (@NotBlank)
    
    Related to #18

---
+++ user/src/main/java/com/high/user/application/dto/request/TokenReissueRequest.java
@@ -0,0 +1,9 @@
+package com.high.user.application.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record TokenReissueRequest(
+        @NotBlank(message = "Refresh Token은 필수입니다")
+        String refreshToken
+) {
+}
