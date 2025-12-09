2f07efcc93da601b85256160a1346978eb83688a
sp-PJS <redvoca100466@gmail.com>
2025-12-09 18:31:05 +0900

    chore: 어노테이션 추가

--- product/src/main/java/com/high/product/ProductApplication.java
+++ product/src/main/java/com/high/product/ProductApplication.java
@@ -2,8 +2,10 @@ package com.high.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableFeignClients
 @SpringBootApplication
 // 공통 라이브러리 스캔 범위 추가
 @ComponentScan(basePackages = {"com.high.product", "com.library.module"})
