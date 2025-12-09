96c5196254cf710eb5bf5bb2ff4fe8ad26cb0acb
sp-PJS <redvoca100466@gmail.com>
2025-12-09 18:29:28 +0900

    chore: config 수정

--- product/src/main/java/com/high/product/infrastructure/config/JpaAuditingConfig.java
+++ product/src/main/java/com/high/product/infrastructure/config/JpaAuditingConfig.java
@@ -12,6 +12,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import com.library.security.util.SecurityContextUtil;
+
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class JpaAuditingConfig {
@@ -19,14 +21,12 @@ public class JpaAuditingConfig {
 	@Bean
 	public AuditorAware<UUID> auditorProvider() {
 		return () -> {
-			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-			if (authentication == null || !authentication.isAuthenticated()) {
+			String userIdString = SecurityContextUtil.getCurrentUserIdAsString();
+			if (userIdString == null || userIdString.isBlank()) {
 				return Optional.empty();
 			}
-
 			try {
-				return Optional.of(UUID.randomUUID()); // userId 저장
+				return Optional.of(UUID.fromString(userIdString));
 			} catch (IllegalArgumentException e) {
 				return Optional.empty();
 			}
--- product/src/main/java/com/high/product/infrastructure/config/SecurityConfig.java
+++ product/src/main/java/com/high/product/infrastructure/config/SecurityConfig.java
@@ -1,52 +1,37 @@
 package com.high.product.infrastructure.config;
 
-// import com.high.product.infrastructure.security.HeaderAuthenticationFilter;
-
-import java.util.Optional;
-import java.util.UUID;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.library.security.filter.HeaderAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
-// @EnableMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 public class SecurityConfig {
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http,
-		DefaultAuthenticationEventPublisher publisher)
-		throws Exception {
-
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-			.csrf(csrf -> csrf.disable())
-			.sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-
-			// .addFilterBefore(new HeaderAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-
-			// .authorizeHttpRequests(auth -> auth
-			// 	.requestMatchers(
-			// 		"/api/v1/**"
-			// 	).permitAll()
-			//
-			// 	.anyRequest().authenticated()
-			// );
+			.csrf(AbstractHttpConfigurer::disable)
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 미사용
 			.authorizeHttpRequests(auth -> auth
-				.anyRequest().permitAll() // 모든 요청 허용
+				.anyRequest().authenticated()
 			);
+
+		http.addFilterBefore(
+			new HeaderAuthenticationFilter(),
+			UsernamePasswordAuthenticationFilter.class
+		);
 
 		return http.build();
 	}
 }
+
+
 
