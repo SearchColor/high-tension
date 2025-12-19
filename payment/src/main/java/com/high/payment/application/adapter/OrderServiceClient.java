63f805122bb926ebb141b66894dcd0e4a6d4bbe5
kinhyunsu <kimhyunsoo0720@gmail.com>
2025-12-20 00:28:32 +0900

    feat: kafka feign 통신에 필요한 파일 및 코드 추가

---
+++ payment/src/main/java/com/high/payment/application/adapter/OrderServiceClient.java
@@ -0,0 +1,16 @@
+package com.high.payment.application.adapter;
+
+import java.util.UUID;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.high.payment.application.port.out.OrderResponse;
+import com.library.module.response.ApiResponse;
+
+@FeignClient(name = "order-serivce", configuration = FeignClient.class)
+public interface OrderServiceClient {
+	@GetMapping("/internal/orders/{orderId}/payment-info")
+	ApiResponse<OrderResponse> getPaymentInfo(@PathVariable("orderId") UUID orderId);
+}
