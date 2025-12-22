b43effff6561172ebb1dbd3f01a4f94ec542cb18
ming <oterges00@gmail.com>
2025-12-22 22:17:59 +0900

    docs: [orchestration] swagger 설정 및 문서 작성

--- orchestration/src/main/java/com/high/orchestration/presentation/SagaController.java
+++ orchestration/src/main/java/com/high/orchestration/presentation/SagaController.java
@@ -13,14 +13,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/orchestrations")
-public class SagaController {
+public class SagaController implements OrchestrationApiDocs{
 
     private final OrderCreateSagaService orderCreateSagaService;
 
