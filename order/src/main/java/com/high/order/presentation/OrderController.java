df4187ff15f570e4c425bc877bf6a2f570587eca
ming <oterges00@gmail.com>
2025-12-22 22:07:04 +0900

    docs: [order] swagger 설정 및 문서 작성

--- order/src/main/java/com/high/order/presentation/OrderController.java
+++ order/src/main/java/com/high/order/presentation/OrderController.java
@@ -2,7 +2,6 @@ package com.high.order.presentation;
 
 import com.high.order.application.OrderService;
 import com.high.order.application.OrderServiceV2;
-import com.high.order.application.dto.request.OrderCreateRequest;
 import com.high.order.application.dto.request.OrderItemDeliveryStatusChangeRequest;
 import com.high.order.application.dto.request.OrderItemStatusChangeRequest;
 import com.high.order.application.dto.request.OrderStatusChangeRequest;
@@ -25,38 +24,37 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/orders")
-public class OrderController {
+public class OrderController implements OrderApiDocs{
 
     private final OrderService orderServiceV1; //주문 생성 테스트용 (order service의 createOrder만 사용)
     private final OrderServiceV2 orderService;
 
+    //deprecated - orchestration으로 이관
+//    @PreAuthorize("hasRole('USER')")
+//    @PostMapping("/product")
+//    public ResponseEntity<ApiResponse<OrderResponse>> createOrder(@Valid @RequestBody OrderCreateRequest productOrderCreateRequest
+//       ) {
+//        UUID userId = SecurityContextUtil.getCurrentUserId();
+//        String userRole = SecurityContextUtil.getCurrentUserRole();
+//        log.info("userId: {}, userRole: {}", userId, userRole);
+//        OrderResponse response = orderServiceV1.createOrder(productOrderCreateRequest, userId, userRole);
+//        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(response));
+//    }
 
-    @PreAuthorize("hasRole('USER')")
-    @PostMapping("/product")
-    public ResponseEntity<ApiResponse<OrderResponse>> createOrder(@Valid @RequestBody OrderCreateRequest productOrderCreateRequest
-       ) {
-        UUID userId = SecurityContextUtil.getCurrentUserId();
-        String userRole = SecurityContextUtil.getCurrentUserRole();
-        log.info("userId: {}, userRole: {}", userId, userRole);
-        OrderResponse response = orderServiceV1.createOrder(productOrderCreateRequest, userId, userRole);
-        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(response));
-    }
 
-    @PreAuthorize("hasAnyRole('USER', 'MASTER')")
-    @PostMapping("/limited-product")
-    public ResponseEntity createLimitedProductOrder() {
-        return ResponseEntity.ok().build();
-    }
-
+    //deprecated - orchestration으로 이관
+//    @PreAuthorize("hasAnyRole('USER', 'MASTER')")
+//    @PostMapping("/limited-product")
+//    public ResponseEntity createLimitedProductOrder() {
+//        return ResponseEntity.ok().build();
+//    }
 
     @PreAuthorize("hasAnyRole('USER', 'SELLER', 'MASTER')")
     @GetMapping("/{orderId}")
@@ -131,7 +129,6 @@ public class OrderController {
 
     //주문 아이템 배송 상태 변경
     @PreAuthorize("hasAnyRole('SELLER', 'MASTER')")
-
     @PatchMapping("/{orderId}/items/{orderItemId}/delivery/status")
     public ResponseEntity<ApiResponse<OrderItemIdResponse>> changeOrderItemDeliveryStatus(
                         @PathVariable UUID orderId, @PathVariable UUID orderItemId, @RequestBody @Valid OrderItemDeliveryStatusChangeRequest request) {
