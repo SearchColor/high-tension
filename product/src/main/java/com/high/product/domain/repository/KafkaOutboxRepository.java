9710b900fcd766d14bbd002faa33619bd8ee9180
sp-PJS <redvoca100466@gmail.com>
2025-12-22 10:34:22 +0900

    feat: 프로듀서 아웃박스 패턴 구현

---
+++ product/src/main/java/com/high/product/domain/repository/KafkaOutboxRepository.java
@@ -0,0 +1,14 @@
+package com.high.product.domain.repository;
+
+import java.util.List;
+
+import com.high.product.domain.model.KafkaOutbox;
+
+public interface KafkaOutboxRepository {
+
+	List<KafkaOutbox> findTop100ByStatusOrderByCreatedAtAsc(String status);
+
+	KafkaOutbox save(KafkaOutbox outbox);
+
+	void markAsSent(KafkaOutbox outbox);
+}
